79e55acb5f4b7b2aa1cfe31af6d264f1717a5aa6
andgitisaac <andgitisaac@gmail.com>
2018-04-22 21:04:40 +0800

    remove unnecessary functions

--- hw3/ops.py
+++ hw3/ops.py
@@ -2,10 +2,8 @@ from __future__ import print_function, division
 
 import random
 import numpy as np
-from keras.metrics import binary_crossentropy
-import keras.backend as K
 from utils import get_file_paths, get_image, vgg_sub_mean, mask_preprocess, mask_postprocess
-import matplotlib.pyplot as plt
+
 def batch_gen(dir, batch_size):
     content_path, mask_path = get_file_paths(dir)    
     content_path.sort()
@@ -13,26 +11,15 @@ def batch_gen(dir, batch_size):
 
     while True:
         index = random.sample(range(1, len(content_path)), batch_size)
-        # for i in index:
-            # print(content_path[i], mask_path[i])
-
         try:
             contents = [vgg_sub_mean(get_image(content_path[i])) for i in index]
             masks = [mask_preprocess(get_image(mask_path[i])) for i in index]
 
             contents = np.asarray(contents, dtype=np.float32)
             masks = np.asarray(masks, dtype=np.float32)
-            # contents = np.asarray(contents, dtype=np.uint8)
-            # masks = np.asarray(masks, dtype=np.uint8)
 
         except Exception as err:
             print("\nError: {}".format(err))
             continue
 
         yield contents, masks
-
-def binary_crossentropy_with_logits(ground_truth, predictions):
-    return K.mean(K.binary_crossentropy(ground_truth,
-                                        predictions,
-                                        from_logits=True),
-                  axis=-1)
--- hw3/train.py
+++ hw3/train.py
@@ -8,16 +8,16 @@ if __name__ == '__main__':
     train_path = 'data/train/'
     val_path = 'data/validation/'
     vgg_path = 'vgg16_weights_tf_dim_ordering_tf_kernels.h5'
-    batch_size = 3
+    batch_size = 5
     steps = 2313 // batch_size
-    epochs = 25
+    epochs = 60
 
     # vgg_fcn = VGG_FCN32(batch_size, mode='train', epochs=epochs, steps=steps, train_dir=train_path, val_dir=val_path, vgg_path=vgg_path)
     # vgg_fcn.train()
 
-    vgg_fcn = VGG_FCN8(batch_size, mode='train', epochs=epochs, steps=steps, train_dir=train_path, val_dir=val_path, vgg_path=vgg_path)
-    vgg_fcn.train()
+    # vgg_fcn = VGG_FCN8(batch_size, mode='train', epochs=epochs, steps=steps, train_dir=train_path, val_dir=val_path, vgg_path=vgg_path)
+    # vgg_fcn.train()
 
-    # vgg_unet = VGG_UNET(batch_size, mode='train', epochs=epochs, steps=steps, train_dir=train_path, val_dir=val_path, vgg_path=vgg_path)
-    # vgg_unet.train()
+    vgg_unet = VGG_UNET(batch_size, mode='train', epochs=epochs, steps=steps, train_dir=train_path, val_dir=val_path, vgg_path=vgg_path)
+    vgg_unet.train()
 
