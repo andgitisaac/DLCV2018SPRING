5a4689b08247423e6fcaa2f3d5bd5123e9d4169a
andgitisaac <andgitisaac@gmail.com>
2018-04-24 21:34:07 +0800

    remove segnet

--- hw3/train.py
+++ hw3/train.py
@@ -3,7 +3,7 @@ from __future__ import print_function, division
 import argparse
 import numpy as np
 from ops import batch_gen
-from model import VGG_FCN32, VGG_FCN8, VGG_UNET
+from model import VGG_FCN32, VGG_FCN8
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--net', type=str,
@@ -15,9 +15,6 @@ if __name__ == '__main__':
     train_path = 'data/train/'
     val_path = 'data/validation/'
     vgg_path = 'vgg16_weights_tf_dim_ordering_tf_kernels.h5'
-    # batch_size = 5
-    # steps = 2313 // batch_size
-    # epochs = 60
 
     if args.net == 'fcn32':
         batch_size = 3
@@ -29,11 +26,6 @@ if __name__ == '__main__':
         steps = 2313  // batch_size
         epochs = 40
         model = VGG_FCN8(batch_size, mode='train', epochs=epochs, steps=steps, train_dir=train_path, val_dir=val_path, vgg_path=vgg_path)
-    elif args.net == 'segnet':
-        batch_size = 5
-        steps = 2313 // batch_size
-        epochs = 60
-        model = VGG_UNET(batch_size, mode='train', epochs=epochs, steps=steps, train_dir=train_path, val_dir=val_path, vgg_path=vgg_path)
-    
+
     model.train()
 
