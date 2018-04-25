1f84b8f3ace43fdf78fe00a149ac5779b1666f97
andgitisaac <andgitisaac@gmail.com>
2018-04-25 19:56:31 +0800

    add script of downloading model

--- hw3/hw3.sh
+++ hw3/hw3.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+gURL="1uuIws2J4IG94CkP12Bu8cNLvOrArCgc9"
+# match more than 26 word characters  
+ggID=$(echo "$gURL" | egrep -o '(\w|-){26,}')
+
+ggURL='https://drive.google.com/uc?export=download'
+
+curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" >/dev/null  
+getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"
+
+cmd='curl --insecure -C - -LOJb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}"'
+echo -e "Downloading from "$gURL"...\n"
+eval $cmd
+
 python3 test.py --net fcn32 --model-path VGG_FCN32.h5 --input-path $1 --output-path $2
--- hw3/hw3_best.sh
+++ hw3/hw3_best.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+gURL="1QLG7PrGS1qwtdyRzItSmkGCaQQClt_dV"
+# match more than 26 word characters
+ggID=$(echo "$gURL" | egrep -o '(\w|-){26,}')
+
+ggURL='https://drive.google.com/uc?export=download'
+
+curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" >/dev/null
+getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"
+
+cmd='curl --insecure -C - -LOJb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}"'
+echo -e "Downloading from "$gURL"...\n"
+eval $cmd
+
 python3 test.py --net fcn8 --model-path VGG_FCN8.h5 --input-path $1 --output-path $2
